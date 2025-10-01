76cf4447d7c5ce0a9dab764ad079daf94ed76a29
Boris Ralichkov <boris.ralichkov@dext.com>
2025-10-01 16:58:09 +0300

    fixup 3

--- kpo.py
+++ kpo.py
@@ -2,30 +2,29 @@ import pendulum
 from airflow.sdk import DAG
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 
-class CustomKpo:
-    def __init__(self) -> KubernetesPodOperator:
-        kpo = KubernetesPodOperator(
-            task_id="idle",
-            name="idle",
-            namespace="airflow",
-            image="bash:5.2",
-            cmds=["bash", "-c"],
-            arguments=["for i in $(seq 1 600); do echo tick $i; sleep 1; done"],
-            in_cluster=True,
-            on_finish_action="delete_pod",
-            reattach_on_restart=True,
-            termination_grace_period=60,
-            termination_message_policy="FallbackToLogsOnError",
-        )
+def create_custom_kpo():
+    kpo = KubernetesPodOperator(
+        task_id="idle",
+        name="idle",
+        namespace="airflow",
+        image="bash:5.2",
+        cmds=["bash", "-c"],
+        arguments=["for i in $(seq 1 600); do echo tick $i; sleep 1; done"],
+        in_cluster=True,
+        on_finish_action="delete_pod",
+        reattach_on_restart=True,
+        termination_grace_period=60,
+        termination_message_policy="FallbackToLogsOnError",
+    )
 
-        def kill_override(self):
-            print("Operator got killed.")
-            self.log.warning("Operator killed.")
-            return
+    def kill_override(self):
+        print("Operator got killed.")
+        self.log.warning("Operator killed.")
+        return
 
-        kpo.on_kill = kill_override
+    kpo.on_kill = kill_override
 
-        return kpo
+    return kpo
 
 
 with DAG(
@@ -34,4 +33,4 @@ with DAG(
     schedule=None,
     default_args={"retries": 0},
 ):
-    op = CustomKpo()
+    op = create_custom_kpo()
