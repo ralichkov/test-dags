3720ad289f826c20089698275743c18c4dfbf86c
Boris Ralichkov <boris.ralichkov@dext.com>
2025-10-01 16:46:51 +0300

    Attempt 1: Override on_kill method

--- kpo.py
+++ kpo.py
@@ -2,22 +2,30 @@ import pendulum
 from airflow.sdk import DAG
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 
+def kill_override(self):
+    print("Operator got killed.")
+    self.log.warning("Operator killed.")
+    return
+
+kpo = KubernetesPodOperator(
+    task_id="idle",
+    name="idle",
+    namespace="airflow",
+    image="bash:5.2",
+    cmds=["bash", "-c"],
+    arguments=["for i in $(seq 1 600); do echo tick $i; sleep 1; done"],
+    in_cluster=True,
+    on_finish_action="delete_pod",
+    reattach_on_restart=True,
+    termination_grace_period=60,
+    termination_message_policy="FallbackToLogsOnError",
+)
+kpo = kpo.on_kill = kill_override
+
 with DAG(
     dag_id="kpo",
     start_date=pendulum.datetime(2016, 1, 1),
     schedule=None,
     default_args={"retries": 0},
 ):
-    KubernetesPodOperator(
-        task_id="idle",
-        name="idle",
-        namespace="airflow",
-        image="bash:5.2",
-        cmds=["bash", "-c"],
-        arguments=["for i in $(seq 1 600); do echo tick $i; sleep 1; done"],
-        in_cluster=True,
-        on_finish_action="delete_pod",
-        reattach_on_restart=True,
-        termination_grace_period=60,
-        termination_message_policy="FallbackToLogsOnError",
-    )
+    kpo
